5231e1cefa9953a92d5e030300bafa5ee1e0da89
obickford <bickfoor@miamioh.edu>
2022-05-01 19:05:18 -0400

    final code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Midterm-Take-Home
+
+  This graph has the shape of a logarithmic function, however it is broken up
+into pieces and each step gets longer and longer as the maximum value gets 
+higher. Because the maximum value gets higher the sequence length stays the
+same since the value before doesn't reach the max value. Once it reaches the
+maximum sequence it levels out and reaches a maximum it stops the length of
+sequence. But at the smaller values of the sequence it increases faster. 
--- tribonacci.R
+++ tribonacci.R
@@ -20,26 +20,26 @@
 ##   the graph looked like. The plot uses the maximum value of the sequence and the
 ##   and the length of the sequence. 
 ##
-##
+##  
 
 ######
 ## Code sequence for part 1
 ######
 
 Tribonacci <- function(a1=0,a2=0,a3=1,min=0,max=30){
-  a4 <- a1 + a2 + a3
-  resultVector <- c(a1,a2,a3)
+  a4 <- a1 + a2 + a3 ##set a4 variable 
+  resultVector <- c(a1,a2,a3) ## create the vector 
   while((a4 >= min)&(a4 <= max)){ ##restricting the sequence to in between the set min and max
     resultVector <- c(resultVector,a4)
     a1 <- a2
-    a2 <- a3
+    a2 <- a3 ##sets the values to increase and continue the sequence
     a3 <- a4
     a4 <- a1 + a2 + a3
   }
 
-  print(resultVector)
+  print(resultVector) #prints the sequence 
   
-  print(length(resultVector))
+  print(length(resultVector))  # prints the length of the sequence 
 }
 
 Tribonacci()
@@ -50,38 +50,36 @@ Tribonacci(a1=0,a2=-4,a3=2,min=-60,max=30)
 ## Part 2 Code
 #######
 
-Tribonacci_max_values <- function(a1,a2,a3,max){
+Tribonacci_max_values <- function(a1=0,a2=0,a3=1,max=30){ 
   a4 <- a1 + a2 + a3
   resultVector <- c(a1,a2,a3)
-  while(a4 <= max){ ##restricting the sequence to in between the set min and max
+  while(a4 <= max){ ##restricting the sequence to just the max value
     resultVector <- c(resultVector,a4)
     a1 <- a2
     a2 <- a3
     a3 <- a4
     a4 <- a1 + a2 + a3
   }
-  length(resultVector)
+  length(resultVector) ## use same function but only the length 
 }
 
-graph_trib <- c()
-for(i in 2:10000) {
-  graph_trib <- c(graph_trib, Tribonacci_max_values(1,-3,2,i))
-}
-plot(graph_trib, main = "Maximum Value Compared to Length of Sequence",
+graph_trib <- c() #sets up empty vector 
+for(i in 2:10000) { # set up the max values to be 2-10000
+  graph_trib <- c(graph_trib, Tribonacci_max_values(a1=1,a2=-3,a3=2,max=i))
+} # fills the empty vector with the function above and the asked values 
+
+plot(graph_trib, main = "Maximum Value Compared to Length of Sequence", #plot with graph labels
      xlab= "maximum value of sequence", ylab= "length of the sequence")
 
-## This graph has the shape of an exponential function, however it is broken up
+
+## This graph has the shape of a logarithmic function, however it is broken up
 ##   into pieces and each step gets longer and longer as the maximum value gets 
-##   higher. 
+##   higher. Because the maximum value gets higher the sequence length stays the
+##   same since the value before doesn't reach the max value. Once it reaches the
+##   maximum sequence it levels out and reaches a maximum it stops the length of
+##   sequence. But at the smaller values of the sequence it increases faster. 
+
 
 ## Source on how to use the plot function:
 ##   https://www.datamentor.io/r-programming/plot-function/
-
-
-
-
-
-
-
-
 
